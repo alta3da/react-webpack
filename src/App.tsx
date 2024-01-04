d2d23c98895fa8f6d8abb103cee849aea39f438d
alta3da <alta3da@gmail.com>
2024-01-04 16:35:28 +0300

    webpack dev & prod configs are made and merged

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,13 @@
+import "./styles.css";
+import Logo from "./react-logo.png";
+import LogoSvg from "./react-logo.svg";
+
 export const App = () => {
-    return <h1> React App by WebPack</h1>
+    return (       
+        <>
+        <h1> React App started by Webpack in {process.env.NODE_ENV} mode</h1>
+         <img src={Logo} width="300"/>
+         <img src={LogoSvg} width="300"/>
+        </>        
+    )
 }
